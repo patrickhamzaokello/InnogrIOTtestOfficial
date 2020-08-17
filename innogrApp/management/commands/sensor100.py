2f9206d77e6242853879cd6d1a806b9a8a46b619
Hafise <pkasemer@gmail.com>
2020-08-17 12:05:35 +0300

    Fixed sensor update

--- innogrApp/management/commands/sensor100.py
+++ innogrApp/management/commands/sensor100.py
@@ -9,7 +9,37 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # collect json data from wazi api
         
-        Sensor.objects.all().delete()
+        urlTC = "https://api.waziup.io/api/v2/sensors_data?sort=dsc&calibrated=true&limit=100&device_id=INNOGRDEVICEPK&sensor_id=InnogrTC"
+        urlHUM = "https://api.waziup.io/api/v2/sensors_data?sort=dsc&calibrated=true&limit=100&device_id=INNOGRDEVICEPK&sensor_id=InnogrHUM"
+        urlWL = "https://api.waziup.io/api/v2/sensors_data?sort=dsc&calibrated=true&limit=100&device_id=INNOGRDEVICEPK&sensor_id=InnogrWL"
+        urlWM = "https://api.waziup.io/api/v2/sensors_data?sort=dsc&calibrated=true&limit=100&device_id=INNOGRDEVICEPK&sensor_id=InnogrWM"
+        urlLI = "https://api.waziup.io/api/v2/sensors_data?sort=dsc&calibrated=true&limit=100&device_id=INNOGRDEVICEPK&sensor_id=InnogrLI"
         
+        sensor = [urlTC,urlHUM,urlWL,urlWM, urlLI]
+        
+        for urllink in sensor:
+            contentsensor = requests.get(urllink)
+            data = contentsensor.json()
+
+            x = len(data)
+            for i in range(x):
+                snid = data[i]['sensor_id']
+                dvid = data[i]['device_id']
+                dateR = data[i]['date_received']
+                timeS = data[i]['timestamp']
+                snval = data[i]['value']
+                # check if url in db
+                try:
+                    # save in db
+                    Sensor.objects.create(
+                        sensorname=snid,
+                        devicename=dvid,
+                        sensorvalue=snval,
+                        date_recieved=dateR,
+                        timestamp=timeS
+                    )
+                    print('%s added' % (snid,))
+                except:
+                    print('%s already exists' % (snid,))
         self.stdout.write( 'Sensor job complete' )
     
